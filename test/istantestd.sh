c2318e1f0cca255770454903b8c00e0158680e36
Pat Sabpisal <ssabpisa@me.com>
2015-02-19 11:54:15 -0500

    Merged with upstream

--- test/istantestd.sh
+++ test/istantestd.sh
@@ -14,6 +14,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+
 function check_success {
   if (( $? > 0 )); then echo "Unable to prepare test environment.." && exit 1; fi
 }
